a78bc58cb4ede16f88a7fb47e08d6026ee11889c
shady2k <shady2k@gmail.com>
2020-01-28 21:56:00 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace lifegame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.udHeight = new System.Windows.Forms.NumericUpDown();
+            this.udWidth = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.udWidth);
+            this.splitContainer1.Panel1.Controls.Add(this.udHeight);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 150;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(646, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // udHeight
+            // 
+            this.udHeight.Location = new System.Drawing.Point(54, 11);
+            this.udHeight.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udHeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.udHeight.Name = "udHeight";
+            this.udHeight.Size = new System.Drawing.Size(59, 20);
+            this.udHeight.TabIndex = 3;
+            this.udHeight.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.udHeight.ValueChanged += new System.EventHandler(this.udHeight_ValueChanged);
+            // 
+            // udWidth
+            // 
+            this.udWidth.Location = new System.Drawing.Point(54, 38);
+            this.udWidth.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.udWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.udWidth.Name = "udWidth";
+            this.udWidth.Size = new System.Drawing.Size(59, 20);
+            this.udWidth.TabIndex = 4;
+            this.udWidth.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.udWidth.ValueChanged += new System.EventHandler(this.udWidth_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Высота";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ширина";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.udWidth)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown udWidth;
+        private System.Windows.Forms.NumericUpDown udHeight;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lifegame
+{
+    public partial class Form1 : Form
+    {
+        bool[,] world;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void GenerateWorld()
+        {
+            GenerateWorld(Int32.Parse(udHeight.Value.ToString()), Int32.Parse(udWidth.Value.ToString()));
+        }
+        private void GenerateWorld(int worldHeight, int worldWidth)
+        {
+            world = new bool[worldWidth, worldHeight];
+
+            int xStep = (pictureBox1.Width - 1) / worldWidth;
+            int yStep = (pictureBox1.Height - 1) / worldHeight;
+
+            int frameHeight = yStep * worldHeight;
+            int frameWidth = xStep * worldWidth;
+
+            Bitmap img = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics graphics = Graphics.FromImage(img))
+            {
+                graphics.FillRectangle(Brushes.White, 0, 0, frameWidth, frameHeight);
+
+                for (int i = 0; i <= worldWidth; i++)
+                {
+                    graphics.DrawLine(Pens.Black, i * xStep, 0, i * xStep, frameHeight);
+                }
+
+                for (int i = 0; i <= worldHeight; i++)
+                {
+                    graphics.DrawLine(Pens.Black, 0, i * yStep, frameWidth, i * yStep);
+                }
+            }
+            pictureBox1.Image = img;
+        }
+        private void UpdateWorld()
+        {
+            int worldHeight = world.GetLength(1);
+            int worldWidth = world.GetLength(0);
+
+            int xStep = (pictureBox1.Width - 1) / worldWidth;
+            int yStep = (pictureBox1.Height - 1) / worldHeight;
+
+            int frameHeight = yStep * worldHeight;
+            int frameWidth = xStep * worldWidth;
+
+            int rectWidth = (frameWidth / worldWidth) - 3;
+            int rectHeight = (frameHeight / worldHeight) - 3;
+
+            Image img = pictureBox1.Image;
+            using (Graphics graph = Graphics.FromImage(img))
+            {
+                for (int i = 0; i < worldWidth; i++)
+                {
+                    for (int j = 0; j < worldHeight; j++)
+                    {
+                        if (world[i, j])
+                        {
+                            graph.FillRectangle(Brushes.Black, i * xStep + 2, j * yStep + 2, rectWidth, rectHeight);
+                        }
+                        else
+                        {
+                            graph.FillRectangle(Brushes.White, i * xStep + 1, j * yStep + 1, rectWidth, rectHeight);
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = img;
+        }
+        private void GenerateRandomPopulation()
+        {
+            Random r = new Random();
+            int x = world.GetLength(0);
+            int y = world.GetLength(1);
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    world[i, j] = (r.NextDouble() >= 0.5);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenerateRandomPopulation();
+            UpdateWorld();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateWorld();
+        }
+
+        private void udHeight_ValueChanged(object sender, EventArgs e)
+        {
+            GenerateWorld();
+        }
+
+        private void udWidth_ValueChanged(object sender, EventArgs e)
+        {
+            GenerateWorld();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            GenerateWorld();
+        }
+    }
+}
